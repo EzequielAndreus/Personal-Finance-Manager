4794558b675cf3697d508f1ade86aa765e2a707d
Javier <ezequiel.andreus@gmail.com>
2025-10-14 23:05:10 -0300

    SCRUM-7 models of Expenses and Debt created

--- main.py
+++ main.py
@@ -8,6 +8,27 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhos
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_pre_ping': True}
 db = SQLAlchemy(app)
 
+# Model of Expense
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Double, nullable=False)
+    categoy = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    element = db.Column(db.String(100), nullable=False)
+    comment = db.Column(db.String(300), nullable=False)
+
+# Model of Debt
+class Debt(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Double, nullable=False)
+    categoy = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    dueDate = db.Column(db.DateTime, nullable=False)
+    element = db.Column(db.String(100), nullable=False)
+    comment = db.Column(db.String(300), nullable=False)
+
 def main():
     print("Hello from milestone-1!")
 
